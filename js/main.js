3610b3db3189894ce84e54a561b5d5378fe2051c
unknown <adminpoli@POLI.local>
2022-04-29 14:22:11 -0300

    Configuração Tablet

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const botaoMenu = document.querySelector ('.icone-menu')
 const menu = document.querySelector ('.menu')
 
 const botaoLupa = document.querySelector ('.icone-lupa')
-const logo = document.querySelector ('.logo')
+const logo = document.querySelector ('.logo__mobile')
 const barradePesquisa = document.querySelector ('.barradepesquisa')
 
 
@@ -13,6 +13,6 @@ botaoMenu.addEventListener ('click', () => {
 });
 
 botaoLupa.addEventListener ('click', () => {
-    logo.classList.toggle ("logo__mobile")
+    logo.classList.toggle ("logo")
     barradePesquisa.classList.toggle ("barradepesquisa__mobile")
 })
